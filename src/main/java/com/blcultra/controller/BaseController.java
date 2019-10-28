7a7aa7d53be1e2e260d3a24df96a7104c6bb3ecb
sgy <sgy0513@163.com>
2019-10-28 09:13:24 +0800

    删除老版本

--- src/main/java/com/blcultra/controller/BaseController.java
+++
@@ -1,9 +0,0 @@
-
-package com.blcultra.controller;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-public class BaseController {
-	protected Logger log = LoggerFactory.getLogger(getClass());
-}
--- src/main/java/com/blcultra/service/DataInitService.java
+++
@@ -1,9 +0,0 @@
-//package com.blcultra.service;
-//
-///**
-// * Created by sgy05 on 2019/8/9.
-// */
-//public interface DataInitService {
-//
-//    String init(String xmlpath);
-//}
